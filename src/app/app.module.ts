c3b56754d80346367b09ec960787f4e4efb2476a
aleksandar <todor.petrovgrad@yahoo.com>
2018-01-09 18:59:20 +0100

    10 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
 import { LayoutComponent } from './layout/layout.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsListComponent,
-    LayoutComponent
+    LayoutComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
