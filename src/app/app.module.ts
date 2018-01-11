d86e14bec2a4b5201fe889c62594c96ab37a52b9
aleksandar <todor.petrovgrad@yahoo.com>
2018-01-11 19:59:45 +0100

    13 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ContactsListComponent } from './contacts-list/contacts-list.component';
-import { LayoutComponent } from './layout/layout.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MessagesComponent } from './components/messages/messages.component';
+
+const appRoutes: Routes = [
+  { path: '', component: ContactsListComponent },
+  { path: 'contacts', component: ContactsListComponent },
+  { path: 'contacts/:id', component: ContactsListComponent },
+   {path: 'messages',component: MessagesComponent,}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsListComponent,
     LayoutComponent,
-    NavBarComponent
+    NavBarComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   
   providers: [],
